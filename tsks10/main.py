b6a5fa1cd3e26299c60e9156253236a25bb077b5
TheZoq2 <frans.skarman@gmail.com>
2017-05-15 19:52:38 +0200

    Find echo start

--- tsks10/main.py
+++ tsks10/main.py
@@ -14,6 +14,7 @@ SAMPLE_RATE = 400 * 10**3
 SAMPLE_SPACING = 1 / SAMPLE_RATE
 
 CARRIER_FREQ = 150 * 10**3
+#CARRIER_FREQ = 113 * 10**3
 
 CUTOFF_FREQ = 5000
 
@@ -65,15 +66,38 @@ def remove_carrier_frequency(sample_rate, data, carrier_freq):
 
     return filtered
 
+#Returns the sample at which the echo starts affecting the signal
 def find_echo_delay(data):
-    samples_until_echo_start = 1000
+    # To avoid waiting for the long calculation to finish
+    return 164003
+
+    samples_until_echo_start = 100000
+
+    #only check the first second of transmission because of the restriction that delay < 500ms
+    samples_until_end_check = 441000
 
     start_samples = data[:samples_until_echo_start]
+    check_samples = data[samples_until_echo_start:samples_until_end_check]
 
-    correlated = np.correlate(data, start_samples, mode='valid')
+    correlated = np.correlate(check_samples, start_samples, mode='valid')
 
-    plot.plot(correlated);
-    #plot.plot(data);
+    correlated /= np.max(np.abs(correlated))
+
+    return correlated.argmax() + samples_until_echo_start
+
+
+def remove_echo(data, echo_start):
+    echo_amplitude = 0.9;
+
+    if len(data) > echo_start*2:
+        print("running echo removal step, len: {}", len(data))
+        clean = data[:echo_start]
+        cleaned = data[echo_start:echo_start*2] - echo_amplitude*clean
+
+        new_data = np.append(cleaned, data[echo_start*2:])
+        return np.append(clean, remove_echo(new_data, echo_start))
+    else:
+        return data
 
 
 def main():
@@ -83,9 +107,18 @@ def main():
 
     interesting_signal = remove_carrier_frequency(fs, data, CARRIER_FREQ)
 
-    find_echo_delay(interesting_signal)
+    normalized_signal = interesting_signal / np.max(np.abs(interesting_signal))
 
-    #plot_fft(transformed, fs, len(data))
+    echo_delay = find_echo_delay(normalized_signal)
+
+    print("Echo delay in seconds: {}, in samples: {}".format(echo_delay / SAMPLE_RATE, echo_delay))
+
+    without_echo = remove_echo(interesting_signal, echo_delay)
+
+    scipy.io.wavfile.write("filtered.wav", SAMPLE_RATE, without_echo / 4000.)
+
+    plot.plot(data)
+    plot.plot(without_echo)
 
 
     plot.show()
