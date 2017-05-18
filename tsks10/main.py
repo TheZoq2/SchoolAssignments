edb2697a51708e546212935c5b641049b6a7bbfd
TheZoq2 <frans.skarman@gmail.com>
2017-05-18 09:13:20 +0200

    Work on iq demodulation

--- tsks10/main.py
+++ tsks10/main.py
@@ -15,6 +15,7 @@ import random
 SAMPLE_RATE = 400 * 10**3
 SAMPLE_SPACING = 1 / SAMPLE_RATE
 
+#CARRIER_FREQ = 75 * 10**3
 CARRIER_FREQ = 150 * 10**3
 #CARRIER_FREQ = 113 * 10**3
 
@@ -44,29 +45,61 @@ def plot_fft(transformed, sample_rate, sample_amount):
 
     plot.plot(xf, 2.0/sample_amount * np.abs(transformed[0:sample_amount // 2]))
 
-
-def remove_carrier_frequency(sample_rate, data, carrier_freq):
+def remove_carrier_frequency(sample_rate, data, carrier_freq, delta=0):
     sample_spacing = 1 / sample_rate
     sample_amount = len(data)
 
     #generate a cosine wave at the correct sample rate
     samples = np.linspace(0.0, sample_amount*sample_spacing, sample_amount)
-    cosine = 2 * np.cos(np.pi * 2 * carrier_freq * samples)
+    cosine = 2 * np.cos(np.pi * 2 * carrier_freq * samples + delta)
+    sine = 2 * np.cos(np.pi * 2 * carrier_freq * samples + delta)
 
     #Multiply the data with the cos wave
-    modulated = data * cosine;
+    modulated_i = data * cosine;
+    modulated_q = data * sine;
 
     #plot_fft(fft(modulated), sample_rate, sample_amount)
 
     #Filter the modulated signal
-    filtered = butter_lowpass_filter(modulated, CUTOFF_FREQ, sample_rate)
+    filtered_i = butter_lowpass_filter(modulated_i, CUTOFF_FREQ, sample_rate)
+    filtered_q = butter_lowpass_filter(modulated_q, CUTOFF_FREQ, sample_rate)
     #plot_fft(fft(filtered), sample_rate, sample_amount)
 
     #plot.plot(filtered)
 
-    scipy.io.wavfile.write("filtered.wav", sample_rate, filtered / 4000.)
+    #scipy.io.wavfile.write("filtered.wav", sample_rate, filtered / 4000.)
 
-    return filtered
+    return (filtered_i, filtered_q)
+
+
+def signal_similarity(signals):
+    (signal1, signal2) = signals
+    # The amount of samples to compare
+    sample_size = 10000
+
+    samples1 = signal1[:sample_size]
+    samples2 = signal2[:sample_size]
+
+    correlated = np.correlate(samples1, samples2, mode='full')
+    plot.plot(correlated)
+    return np.max(correlated)
+
+def find_delta(sample_rate, data, carrier_freq):
+    step_amount = 100
+
+    result = np.linspace(0, 0, step_amount)
+
+    for i in range(0,step_amount):
+        delta = math.pi * 2 * (i/step_amount)
+
+        signals = remove_carrier_frequency(sample_rate, data, carrier_freq, delta)
+
+        similarity = signal_similarity(signals)
+        print(similarity, delta)
+        result[i] = similarity
+
+    plot.plot(result)
+
 
 #Returns the sample at which the echo starts affecting the signal
 def find_echo_delay(data):
@@ -117,16 +150,18 @@ def main():
     (fs, data) = scipy.io.wavfile.read("signal.wav")
 
     transformed = fft(data)
+    #plot_fft(transformed, fs, len(data))
 
-    interesting_signal = remove_carrier_frequency(fs, data, CARRIER_FREQ)
+    find_delta(fs, data, CARRIER_FREQ)
+    interesting_signals = remove_carrier_frequency(fs, data, CARRIER_FREQ, math.pi * 2 * 18/100)
 
-    normalized_signal = interesting_signal / np.max(np.abs(interesting_signal))
+    normalized_signal = interesting_signals[0] / np.max(np.abs(interesting_signals[0]))
 
     echo_delay = find_echo_delay(normalized_signal)
 
     print("Echo delay in seconds: {}, in samples: {}".format(echo_delay / SAMPLE_RATE, echo_delay))
 
-    without_echo = remove_echo(interesting_signal, echo_delay)
+    without_echo = remove_echo(interesting_signals[0], echo_delay)
 
     scipy.io.wavfile.write("no_echo.wav", SAMPLE_RATE, without_echo / 4000.)
 
